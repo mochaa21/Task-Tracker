3725230fd074b180fd0558dff7ce607b780fc9ef
mochaa21 <mochamadsyahid573@gmail.com>
2026-09-02 22:13:53 +0700

    fix: logical sys

--- task_cli.py
+++ task_cli.py
@@ -3,3 +3,15 @@ import json
 from datetime import datetime
 import os
 
+if len(sys.argv) < 2:
+    print("Error: Missing required argument.")
+    print("Usage: python script.py <your_argument>")
+    sys.exit(1)  # Stop execution with an error code
+
+SyalwaSlutt = sys.argv[1]
+
+if SyalwaSlutt == "SyalwaSlutt":
+    if len(sys.argv) < 3:
+        print("Error! Argument 3 is not defined.")
+    else:
+        print(sys.argv)
