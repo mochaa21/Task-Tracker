c63bf7c80e310d08cedfdc723940973def201f94
mochaa21 <mochamadsyahid573@gmail.com>
2026-09-02 08:01:34 +0700

    feat: testing json module

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import sys
+import json
 
 print(f'Python version: {sys.version}')
 print(f'Platform: {sys.platform}')
 print(f'Passed to the script: {sys.argv}')
+print(f'Copyright: {sys.copyright}')
+print(f'Version tuple: {sys.version_info}')
