a56302d252c973562028ebcdac63ffbfe51da5d7
mochaa21 <mochamadsyahid573@gmail.com>
2026-09-03 22:02:25 +0700

    update: logical bug

--- task_cli.py
+++ task_cli.py
@@ -6,7 +6,11 @@ import os
 FILE_NAME = 'tasks.json'
 
 # db code
-
+def load_tasks():
+    if not os.path.exists(FILE_NAME):
+        return []
+    with open(FILE_NAME, 'r') as file:
+        return json.load(file)
 
 def save_tasks(tasks):
     with open(FILE_NAME, 'w') as file:
