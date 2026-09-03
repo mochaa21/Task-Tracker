a69d11a9f28ffdfac169ceae829b8c26852bf628
mochaa21 <mochamadsyahid573@gmail.com>
2026-09-03 12:17:24 +0700

    update: new feature (add task)

--- task_cli.py
+++ task_cli.py
@@ -17,7 +17,22 @@ def save_tasks(tasks):
         return json.dump(tasks, file, indent=4)
 
 # feature
+def add_task(description):
+    tasks = load_tasks()
+    new_id = 1 if len(tasks) == 0 else tasks[-1]['id'] + 1
+    now = datetime.now().isoformat()
 
+    new_task = {
+        'id': new_id,
+        'description': description,
+        'status': 'todo',
+        'createAt': now,
+        'updateAt': now
+    }
+
+    tasks.append(new_task)
+    save_tasks(tasks)
+    print(f"Task added successfully (ID: {new_id})")
 
 if __name__ == "__main__":
     main()
